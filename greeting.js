b5826e8e71a9ac345f2101e80a5d7b2368df45ef
Francis <michaelfrancisoro@gmail.com>
2023-05-10 23:56:37 -0400

    Debug JavaScript greeting on Page 10

--- greeting.js
+++ greeting.js
@@ -1,6 +1,6 @@
 /* Greet the user based on the current time */
 const time = new Date().getHours();
-    let greeting;
+    let greeting
     if (time < 10) {
     greeting = "Good morning";
     } else if (time < 20) {
